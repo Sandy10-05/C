198bc15232d45b4e5fe91019334bf9f9d672c177
Sandy10-05 <313002@s.lhvs.tyc.edu.tw>
2024-12-19 15:27:49 +0800

    Update 20241219建立類別.cpp

--- 20241219建立類別.cpp
+++ 20241219建立類別.cpp
@@ -1,1 +1,23 @@
+#include<iostream>
+using namespace std;
 
+class myTime{
+	public:
+		int hour;
+		int minute;
+		void printTime();
+};
+
+void myTime::printTime(){
+	cout<<hour<<":"<<minute<<"\n";
+}
+int main(){
+	myTime now,open;
+	now.hour=8;
+	now.minute=18;
+	cout<<"現在時間:";
+	open.hour=9;
+	open.minute=10;
+	cout<<"開張時間:";
+	open.printTime();
+}	
